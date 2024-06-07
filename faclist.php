6f14c76fdd9daca28a67105d36c36045221f74c2
Yohana-cmd <150522972+Yohana-cmd@users.noreply.github.com>
2024-06-07 05:07:05 -0700

    Add files via upload
    php files for schedule system

---
+++ faclist.php
@@ -0,0 +1,102 @@
+<?php
+   include_once("header.php");
+   include_once("navbar.php");
+?>
+<html>
+<head>
+    <style>
+      body {
+        background-image: url();
+        background-color: white;
+      }
+      th {
+        text-align: center;
+      }
+      tr {
+         height: 30px;
+      }
+      td {
+          padding-top: 5px;
+        padding-left: 20px; 
+        padding-bottom: 5px;  
+        height: 20px;
+      }
+
+    </style>
+</head>
+<body><br>
+<div class="container">
+
+    
+<body>
+    <?php
+     echo "<tr>
+            <td>";
+               // your database connection
+              $host = "localhost"; 
+              $username   = "root"; 
+              $password   = "";
+              $database   = "insertion"; 
+         
+               // select database
+              $connect = mysqli_connect($host,$username,$password) or die(mysqli_error($connect)); 
+              mysqli_select_db($connect, $database) or die(mysqli_error($connect)); 
+
+                    $query = ("SELECT * FROM faculty");
+                    $result = mysqli_query($connect, $query) or die(mysqli_error($connect));
+                    echo "<div class='container'><table width='' class='table table-bordered' border='1' >
+                            <tr>
+                                <th>Faculty</th>
+                                <th>Designation</th>
+                                 <th>Action</th>
+                            </tr>";
+                        while($row = mysqli_fetch_array($result))
+                        {
+                        echo "<tr>";
+                        echo "<td>" . $row['faculty_name'] . "</td>";
+                        echo "<td>" . $row['designation'] . "</td>";
+                        echo "<td><form class='form-horizontal' method='post' action='faclist.php'>
+                        <input name='faculty_id' type='hidden' value='".$row['faculty_id']."';>
+                        <input type='submit' class='btn btn-danger' name='delete' value='Delete'>
+                        </form></td>";
+                        echo "</tr>";
+                        }
+                    echo "</table>";
+
+            echo "</td>           
+        </tr>";
+
+       // delete record
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+    echo '<script type="text/javascript">
+                      alert("row Successfuly Deleted");
+                         location="list.php";
+                           </script>';
+    }
+    if(isset($_POST['faculty_id']))
+    {
+    $faculty_id = mysqli_real_escape_string($connect, $_POST['faculty_id']);
+    $sql = mysqli_query($connect, "DELETE FROM faculty WHERE faculty_id='$faculty_id'");
+    if(!$sql)
+    {
+        echo ("Could not delete rows" .mysqli_error($connect));
+    }
+  
+    }
+    ?>
+</fieldset>
+</form>
+</div>
+</div>
+</div>
+</div>
+  </body>
+  </html>
+  
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "footer.php";
+   include_once("footer.php");
+
+?>
---
+++ timelist.php
@@ -0,0 +1,100 @@
+<?php
+   include_once("header.php");
+   include_once("navbar.php");
+?>
+<html>
+<head>
+    <style>
+        body {
+        	background-image: url();
+        	background-color: white;
+        }
+        th {
+        	text-align: center;
+        }
+        tr {
+        	 height: 30px;
+        }
+        td {
+            padding-top: 5px;
+        	padding-left: 20px;	
+        	padding-bottom: 5px;	
+        	height: 20px;
+        }
+    </style>
+</head>
+<body><br>
+<div class="container">
+
+<body>
+    <?php
+     echo "<tr>
+            <td>";
+               // your database connection
+			   $host       = "localhost"; 
+               $username   = "root"; 
+               $password   = "";
+               $database   = "insertion"; 
+			   
+               // select database
+			   $connect = mysqli_connect($host,$username,$password) or die(mysqli_error($connect)); 
+               mysqli_select_db($connect, $database) or die(mysqli_error($connect)); 
+
+                    $query = ("SELECT * FROM timer");
+                    $result = mysqli_query($connect, $query) or die(mysqli_error($connect));
+                    echo "<div class='container'><table width='' class='table table-bordered' border='1' >
+                            <tr>
+                             <th>Start time</th>
+								             <th>End time</th>
+                                <th>Action</th>
+                            </tr>";
+                        while($row = mysqli_fetch_array($result))
+                        {
+                        echo "<tr>";
+                        echo "<td>" . $row['start_time'] . "</td>";
+						            echo "<td>" . $row['end_time'] . "</td>";
+                        echo "<td><form class='form-horizontal' method='post' action='timelist.php'>
+                        <input name='id' type='hidden' value='".$row['id']."';>
+                        <input type='submit' class='btn btn-danger' name='delete' value='Delete'>
+                        </form></td>";
+                        echo "</tr>";
+                        }
+                    echo "</table>";
+
+            echo "</td>           
+        </tr>";
+
+       // delete record
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+		echo '<script type="text/javascript">
+                      alert("Schedule Successfuly Deleted");
+                         location="tablelist.php";
+                           </script>';
+    }
+    if(isset($_POST['id']))
+    {
+    $id = mysqli_real_escape_string($connect, $_POST['id']);
+    $sql = mysqli_query($connect, "DELETE FROM timer WHERE id='$id'");
+    if(!$sql)
+    {
+        echo ("Could not delete rows" .mysqli_error($connect));
+    }
+	
+    }
+    ?>
+</fieldset>
+</form>
+</div>
+</div>
+</div>
+</div>
+	</body>
+	</html>
+	
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "footer.php";
+   include_once("footer.php");
+
+?>
